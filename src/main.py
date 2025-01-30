df21605fb954f3559a74a1a095312d33cd7db2b4
TT\llfDL <llfdlm@outlook.com>
2025-01-30 21:31:41 +0800

    UI can work

--- src/main.py
+++ src/main.py
@@ -1,34 +1,35 @@
-import os
-from PyQt5 import QtCore, QtWidgets
+
+from PyQt5 import  QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from src.UI2 import Ui_MainWindow  # 假设 UI 文件位于 src/UI2.py 中
 import sys
+import Prd_M
+import Sht_manager
+#import R2Bom
 
 import win32com.client
 # from pycatia import CATIA
-
 class MainWindow(QMainWindow):
-
-    def r2bom(self, rw):
+    def BTNF(self, rw):
+        #R2Bom.CBM(self,rw)
         pass
-
-
+        
     def __init__(self):
         super().__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         # 动态为每个按钮绑定函数
-        for i in range(1, 7):
+        for i in range(1, 6):
             button_name = f"pushButton_{i}"
             button = getattr(self.ui, button_name, None)
             if button:
-                button.clicked.connect(lambda checked, rw=i: self.r2bom(rw))
+                button.clicked.connect(lambda checked, rw=i: self.BTNF(rw))
 
 def create_ui():
-    app = QApplication(sys.argv)
-    window = MainWindow()
-    window.show()
-    sys.exit(app.exec_())
+        app = QApplication(sys.argv)
+        window = MainWindow()
+        window.show()
+        sys.exit(app.exec_())
 
 if __name__ == "__main__":
-    create_ui()
+        create_ui()
