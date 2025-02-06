8915ffc32bf06bea6d7f3f9597c2382a892ae206
verysolecd <verysolecd@hotmail.com>
2025-02-06 16:00:46 +0800

    new file:  Python作弊代码

---
+++ docs/作弊代码.py
@@ -0,0 +1,50 @@
+# 创建虚拟环境
+python -m venv .env
+# 激活虚拟环境（Windows）
+.\.env\Scripts\activate
+
+# 安装包，-e表示便携安装
+pip install -e .
+
+
+
+
+pip install -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple/
+
+
+
+目前国内比较好用的pypi源有：
+
+http://mirrors.aliyun.com/pypi/simple/          阿里云
+
+http://pypi.douban.com/simple/                     豆瓣
+
+https://pypi.mirrors.ustc.edu.cn/simple/         中国科技大学
+
+http://pypi.mirrors.opencas.cn/simple/           中科院
+
+https://pypi.tuna.tsinghua.edu.cn/simple/       清华大学
+
+
+
+
+
+
+GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG
+# 初始化本地Git仓库
+git init
+# 添加远程仓库地址
+git remote add origin <GitHub仓库地址>
+# 拉取远程仓库代码（如果有）
+git pull origin main
+
+
+# 添加更改到暂存区
+git add .
+# 提交更改
+git commit -m "调试并更新代码"
+# 推送到GitHub
+git push origin main
+
+
+GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG
